71b431c0d525a559d3911125ea0c47b036f4c30d
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2024-11-08 00:41:44 +0700

    Fix DestinationSelector component

--- src/components/event/form/destination-selector/DestinationSelector.jsx
+++ src/components/event/form/destination-selector/DestinationSelector.jsx
@@ -11,29 +11,25 @@ export default function DestinationSelector({destination, type, setPointState}) 
 
   function handleDestinationСhange(evt) {
     const newDestination = mockDestinations.find((element) => element.name === evt.target.value);
-    if (newDestination) {
-      setPointState((point) => ({ ...point, destination: newDestination.id }));
-    }
+    setPointState((point) => ({ ...point, destination: newDestination.id }));
     setInputValue(evt.target.value);
   }
 
   return (
     <div className={cn(lcs.eventFieldGroup, lcs.eventFieldGroupDestination)}>
       <label className={lcs.eventLabel} htmlFor="event-destination">{type}</label>
-      <input
+      <select
         className={cn(lcs.eventInput, lcs.eventInputDestination)}
         id="event-destination"
-        type="text"
         name="event-destination"
         value={inputValue}
-        list="destination-list"
         onChange={handleDestinationСhange}
-      />
-      <datalist id="destination-list">
+        required
+      >
         {mockDestinations.map((element) => (
-          <option key={element.id} value={element.name}></option>
+          <option key={element.id} value={element.name}>{element.name}</option>
         ))}
-      </datalist>
+      </select>
     </div>
   )
 }
